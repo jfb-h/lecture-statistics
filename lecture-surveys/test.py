5b1ff486de019799e02c00eb33d2e97efab3d3bc
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2024-09-10 14:38:18 +0000

    uv lockfile

--- lecture-surveys/test.py
+++ lecture-surveys/test.py
@@ -2,11 +2,21 @@ from fasthtml.common import *
 
 app, rt = fast_app(live=True)
 
-@rt('/sess/test')
-def get(): return Div(P('Hello World!'), hx_post='/statlecture/sess/test')
+def route1():
+    @rt('/sess/test')
+    def get(): return Div(P('Hello World!'), hx_post='/statlecture/sess/test')
 
-@rt('/sess/test')
-def get(): return P('Nice to be here!')
+    @rt('/sess/test')
+    def post(): return P('Nice to be here!')
 
+def route2():
+    @rt('/sess/test2')
+    def get(): return Div(P('Hello Moon!'), hx_post='/statlecture/sess/test2')
+
+    @rt('/sess/test2')
+    def post(): return P('Nice to be on the moon!')
+
+route1()
+route2()
 serve(port=8081)
 
