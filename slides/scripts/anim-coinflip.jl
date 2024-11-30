7582ec20787751992e17b136468c024d5ae72864
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2024-11-30 13:43:23 +0100

    stat models

---
+++ slides/scripts/anim-coinflip.jl
@@ -0,0 +1,14 @@
+using GLMakie
+using Makie.Colors
+
+fig, ax, lineplot = lines(0 .. 10, sin; linewidth=10)
+
+# animation settings
+nframes = 30
+framerate = 30
+hue_iterator = range(0, 360, length=nframes)
+
+record(fig, "color_animation.mp4", hue_iterator;
+    framerate=framerate) do hue
+    lineplot.color = HSV(hue, 1, 0.75)
+end
